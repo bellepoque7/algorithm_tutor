f507900ad7481b893cc512321ffd248cb7bea70a
mijinnnnn <128116347+mijinnnnn@users.noreply.github.com>
2023-03-21 19:57:56 +0900

    Update and rename 권미진_실패율 to 권미진_실패율.py

--- 03.22(수)/권미진_실패율.py
+++ 03.22(수)/권미진_실패율.py
@@ -1,3 +1,5 @@
+#1 (조금 더 빠름)
+
 def solution(N, stages):
 
     temp = []
@@ -19,5 +21,25 @@ def solution(N, stages):
     if len(answer) < N:
         for i in range(test_range,N+1):
             answer.append(i)
+
+    return answer
+
+#2
+def solution(N, stages):
+    temp = []
+    players = len(stages)
+
+    for i in range(1, N + 1):
+        if players == 0:
+            temp.append((i, 0))
+
+        else:
+            cur_p = stages.count(i)
+            f_rate = cur_p / players
+            temp.append((i, f_rate))
+            players -= cur_p
+
+    temp = sorted(temp, key=lambda x: (x[1], -x[0]), reverse=True)
+    answer = [x[0] for x in temp]
 
     return answer
