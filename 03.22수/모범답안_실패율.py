f798edff47155da93c7ebf9131928588af619ceb
Lim Jung <39439424+bellepoque7@users.noreply.github.com>
2023-03-22 16:09:42 +0900

    Update 모범답안_실패율.py

--- 03.22수/모범답안_실패율.py
+++ 03.22수/모범답안_실패율.py
@@ -8,17 +8,17 @@ def solution(N, stages):
     #스테이지 번호를 1부터 N까지 증가시킴
     for i in range(1,N+1):
         #해당 스테이지에 머물러 있는 사람의 수 계산
-        count = stages.count(i)
+        cnt = stages.count(i)
         
         #실패율 계산
         if length ==0:
             fail = 0
         else:
-            fail = count / length
+            fail = cnt / length
         
         #리스트에 (스테이지 번호, 실패율) 삽입
         answer.append(i,fail)
-        length -= count
+        length -= cnt
 
     #실패율을 기준으로 각 스테이지를 내림차순 정렬
     answer = sorted(answer, key = lambda t: t[1], reverse=True)
