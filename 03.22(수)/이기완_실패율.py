d0bc39cf62e11998af366f8e8cff4c8b182efa73
Giwanlee <128117968+Giwanlee@users.noreply.github.com>
2023-03-22 09:46:08 +0900

    Create 이기완_실패율.py

---
+++ 03.22(수)/이기완_실패율.py
@@ -0,0 +1,28 @@
+def solution(N, stages):
+    stages.sort()
+    temp = 0
+    result = [[] for i in range(N)]
+    answer = []
+    total = 0
+    for i in range(1,N+1) :
+        for j in range(len(stages)) :
+            if stages[j] < i :
+                continue
+            
+            elif stages[j] == i :
+                temp += 1
+                total += 1
+            elif stages[j] > i :
+                total += 1
+        result[i-1].append(i)
+        result[i-1].append(temp / total)
+        total = 0
+        temp = 0
+
+    result = sorted(result, key = lambda x : (x[1]), reverse = True)
+    
+    for i in range(N) :
+        answer.append(result[i][0])
+    
+
+    return answer
