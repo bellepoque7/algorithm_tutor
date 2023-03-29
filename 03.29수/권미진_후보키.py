080092b7b04fa50b0321f1f738116ec336d06e95
mijinnnnn <128116347+mijinnnnn@users.noreply.github.com>
2023-03-29 19:32:43 +0900

    Update 권미진_후보키.py

--- 03.29수/권미진_후보키.py
+++ 03.29수/권미진_후보키.py
@@ -46,3 +46,53 @@ def check(comb,relation):
     
     return len(li_s) == len(li)
 
+
+#내장함수 사용하기
+----------------------------------------------------------------------------------------------------
+from itertools import combinations
+
+def solution(relation):
+    
+    a = len(relation[0])
+    
+    
+    # if a == 1:
+    #     return 1
+    
+    res = []
+    li = combi(a) #칼럼번호들의 조합
+    
+    
+    for comb in li:
+        flag = True  #최소성 검증
+        for i in res:
+            if len(comb) == 1:
+                continue
+            if set(i).issubset(set(comb)): #부분집함임을 확인하기
+                flag = False
+        
+        if flag and check(comb,relation):
+                res.append(comb)
+    print(res)
+    answer = len(res)
+    return answer
+
+
+def combi(a):
+    comb = []
+    for i in range(1,a+1):
+        comb += list(combinations(range(a),i))
+    return comb    
+
+#유일성 검증
+def check(comb,relation):  #리스트를 tuple로 변환하여 요소로 넣으면 set 함수 사용가능
+    li = []  
+    for i in relation:
+        temp = []
+        for j in comb:
+            temp.append(i[j])
+        li.append(tuple(temp))
+    
+    
+    return len(set(li)) == len(li)
+
