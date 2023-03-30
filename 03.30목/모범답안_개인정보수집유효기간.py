9a69c2ceb7419e1657dbc8c1db2d7faa36edc809
Lim Jung <39439424+bellepoque7@users.noreply.github.com>
2023-03-30 15:35:35 +0900

    update

--- 03.30목/모범답안_개인정보수집유효기간.py
+++ 03.30목/모범답안_개인정보수집유효기간.py
@@ -2,6 +2,7 @@
 
 def to_days(date):
     year, month, day = map(int, date.split("."))
+    #날짜를 숫자단위로 변환
     return year * 28 * 12 + month * 28 + day
 
 def solution(today, terms, privacies):
@@ -9,10 +10,14 @@ def solution(today, terms, privacies):
     months = {v[0]: int(v[2:]) * 28 for v in terms}
     # print(months) # {'A': 168, 'B': 336, 'C': 84},{'Z': 84, 'D': 140}
     today = to_days(today)
+    print(today)
+    # privacies의 루프에서, 만약 날짜 값과 기간으 더한 값이 현재 기준보다 낮은 경우에만 
+    #리스트에 넣어라. 1을 더해서(index니까)
     expire = [
         i + 1 for i, privacy in enumerate(privacies)
         if to_days(privacy[:-2]) + months[privacy[-1]] <= today
     ]
+    print(expire)
     return expire
 
 # today = "2022.05.19"
